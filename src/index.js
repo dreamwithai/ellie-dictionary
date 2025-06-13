a87d3f2ecb83401907a29055387140dafaf15246
dreamwithai <dev.dreamwithai@gmail.com>
2025-06-13 19:20:46 +0900

    PWA 경로 /ellie-dictionary/로 원복 (github pages 호환)

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
 // Service Worker 등록
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('/ellie-dictionary/sw.js')
       .then(registration => {
         console.log('Service Worker 등록 성공:', registration.scope);
       })
